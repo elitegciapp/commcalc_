f54ec1b5a0134c2722ebfc98a535491c22602bb0
maincaz <maincaz@gmail.com>
2025-12-29 14:10:10 -0500

    Fix Radix toast usage and remove Toaster

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+import { queryClient } from "./lib/queryClient";
+
+import {
+  ToastProvider,
+  ToastViewport,
+} from "@components/ui/toast";
+
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 import Splash from "@/pages/Splash";
@@ -20,13 +25,16 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <AnimatedBackground />
-      <div className="relative z-10">
-        <Toaster />
-        <Router />
-      </div>
+      <ToastProvider>
+        <AnimatedBackground />
+        <div className="relative z-10">
+          <Router />
+          <ToastViewport />
+        </div>
+      </ToastProvider>
     </QueryClientProvider>
   );
 }
 
 export default App;
+
