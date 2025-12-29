3a3c47f01a903dc123ec0e9a576bcbee1461fd26
elitegciapp <elitegciapp@gmail.com>
2025-12-29 14:04:47 -0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+import { Toast } from "@/components/ui/toast";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 import Splash from "@/pages/Splash";
